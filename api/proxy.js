f6dd94777a2dcf43ce584d678d9822f1bac1cbdc
Hanson-zhy <157664656+Hanson-zhy@users.noreply.github.com>
2025-03-06 22:08:17 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.serv00.com/";//your website url
+  let target = "https://www.suno.com/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
